298f009e071986fb9f5ba8199e6585e6aec1e4c9
Tushar Ojha <tusharojha2001@gmail.com>
2023-12-13 11:22:32 +0530

    added logic for code generation & Grill integration

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import { useAppDispatch, useAppSelector } from "@/redux/hooks"
 import { setConfig, setLoading, setManifest, setNef } from "@/redux/slice"
-import axios from "axios"
+
+import grill from '@subsocial/grill-widget'
 
 export const Header = () => {
 
@@ -12,14 +13,19 @@ export const Header = () => {
   return <div className="navbar bg-base-100">
     <div className="flex-1">
       <a className="btn btn-ghost normal-case text-xl">
-        <h1 className='font-bold text-2xl flex flex-row items-center'>Go <img src="/neo.png" className='h-12' /></h1>
+        <h1 className='font-bold text-2xl flex flex-row items-center'><img src="/speedink-logo.png" className='h-12' /></h1>
       </a>
       fastest way to go live 🚀
     </div>
     <div className="flex-none">
       <ul className="menu menu-horizontal px-1 font-bold text-lg">
-        <li><a className="font-normal mr-2 boder-2 hover:bg-white">Testnet</a></li>
-        <li><a className="font-normal mr-2 boder-2 hover:bg-white">Ask AI</a></li>
+        <li><a onClick={() => {
+
+          const config = {
+            hub: { id: '' },
+          }
+          grill.init(config as any)
+        }} className="font-normal mr-2 boder-2 hover:bg-white">Ask Chat</a></li>
         <li><a className="border-2 border-accent">{loading ? 'Loading..' : 'Compile'}</a></li>
       </ul>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import { setConfig, setManifest, setNef } from '@/redux/slice'
 
 import axios from 'axios'
 import { HomeView } from '@/components/HomeView';
+import { CodeScreen } from '@/components/CodeScreen';
+import { useEffect } from 'react';
 
 export default function Home() {
 
@@ -47,5 +49,35 @@ export default function Home() {
     return <HomeView />
   }
 
-  return <></>
+  return <>
+    <CodeScreen />
+    <dialog id="my_modal_2" className="modal">
+      <form method="dialog" className="modal-box flex flex-col">
+        <h3 className="font-bold text-lg">Compile Contract</h3>
+        <p className="py-4">Check & Update the following config.yml</p>
+        <textarea value={config} onChange={(e) => dispatch(setConfig(e.currentTarget.value))} className="textarea textarea-accent h-64" placeholder="Config"></textarea>
+        <button className='btn btn-accent mt-4' onClick={compileNow}>Submit</button>
+      </form>
+      <form method="dialog" className="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+    <div className='fixed-div'>
+      <div id="grill" className='chat'></div>
+      <img onClick={() => {
+        const grillDiv = document.getElementById('grill');
+
+        // Toggle the 'chat-show' class on the div element
+        grillDiv?.classList.toggle('chat-show');
+
+        // Check if the class 'chat-show' is present
+        if (grillDiv?.classList.contains('chat-show')) {
+          // Initializing grill.
+          const config = {};
+          (window as any)?.GRILL?.init(config);
+        }
+      }} id="grill-toggle" alt="grill-logo" src="https://sub.id/images/grillchat.svg" />
+    </div>
+
+  </>
 }
